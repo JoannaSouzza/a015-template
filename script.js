f26feb07a6747be83cf26a8f995b3c8e23ce7622
Joanna Souza <joannasouza40@gmail.com>
2022-08-23 15:02:47 -0300

    Tá osso.

---
+++ script.js
@@ -0,0 +1,21 @@
+/* let usuario = prompt("Qual seu nível de acesso: A (admistrador), B (usuário comum) ou C (assinante)?").toUpperCase()
+
+while(usuario !== "A") {
+    console.log("Acesso negado")
+    usuario = prompt("Qual seu nível de acesso: A (admistrador), B (usuário comum) ou C(assinante)?").toUpperCase()
+}
+console.log(alert("Boas vindas admin!")) */
+
+//Laço tabuada do 2
+
+
+/* for(let resultado = 1; resultado <= 10; resultado++){
+    console.log(resultado*2)
+}
+console.log("Fim") */
+
+let string = ["Joanna","Jojo", "A Jo"]
+
+for(let i=0; i <= string.length; i++){
+    console.log(string[i].toUpperCase())
+}
